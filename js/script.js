205e750785ab6569e448d52fe4a80c6acbe8f7a4
tewff14 <chanat.hat@dome.tu.ac.th>
2023-12-09 20:34:13 +0700

    Fix academic year variable name

--- js/script.js
+++ js/script.js
@@ -216,7 +216,7 @@ async function submitForm(event) {
                 // Validate the start date and end date
                 const startDateInput = data.start_date;
                 const endDateInput = data.end_date;
-                const academicYear = data.academicYear - 543;
+                const academicYear = data.academic_year;
                 const semesters = calendarData[academicYear];
 
                 if (!semesters) {
